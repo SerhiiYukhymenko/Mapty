5f1590caf34c8c099a28be649a6d05896fd029d4
SerhiiYukhymenko <serhiiyukhymenko@gmail.com>
2021-09-13 13:33:09 +0300

    Initial dev branch

--- script.js
+++ script.js
@@ -185,8 +185,6 @@ class App {
     let html = `
     <li class="workout workout--${workout.type}" data-id="${workout.id}">
       <h2 class="workout__title">${workout.description}</h2>
-      <button class="workout__edit-btn">Edit</button>
-      <button class="workout__delete-btn">Delete</button>
       <div class="workout__details">
         <span class="workout__icon">${
           workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'
